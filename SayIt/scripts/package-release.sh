98a346222cbb52df0f40c2c6dd37e68839125c63
牛拙 <prince@niuzhuodeMacBook-Air.local>
2026-02-11 18:08:29 -0500

    chore: bump dmg name to v1.0.3

--- SayIt/scripts/package-release.sh
+++ SayIt/scripts/package-release.sh
@@ -9,7 +9,7 @@ PROJECT_NAME="SayIt"
 SCHEME_NAME="SayIt"
 BUILD_DIR="./build"
 RELEASE_DIR="${BUILD_DIR}/Release"
-DMG_NAME="${PROJECT_NAME}_v1.0.2.dmg"
+DMG_NAME="${PROJECT_NAME}_v1.0.3.dmg"
 
 echo "🚀 Starting productization build..."
 
